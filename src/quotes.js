53eeefb43240391cc2f893b733eb07e8c03cc69a
Suraj Verma <hi@surajverma.in>
2019-11-05 22:23:37 +0530

    next

--- src/quotes.js
+++ src/quotes.js
@@ -9,13 +9,30 @@ class User extends React.Component {
     const { picture, isLoading } = this.props;
 
     const userDetails = (
-      <div>
-        <img
-          className="img-thumbnail mx-auto mb-2 shadow-sm"
-          src={picture}
-          alt={picture}
-          style={{ width: "500px"}}
-        />        
+      // <div>
+      //   <img
+      //     className="img-thumbnail mx-auto mb-2 shadow-sm"
+      //     src={picture}
+      //     alt={picture}
+      //     style={{ width: "500px"}}
+      //   />        
+      // </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="quotes-bg">
+            <div className="quotes-overlay">
+              <div className="quotes-base text-center">
+                <p className="quote">You only live once, but if you do it right, once is enough.<span className="quote-by">Mae West</span></p>					
+              </div>
+              <div className="shuffle text-center">
+                <i className="fa fa-random"></i>
+              </div>
+              <div className="info-btn">
+                <i className="fa fa-info"></i>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
 

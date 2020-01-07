d43df96a644e3c205a56d8b16c322416b1a15ca3
Suraj Verma <hi@surajverma.in>
2020-01-07 11:09:25 +0530

    random backgrounds

--- src/quotes.js
+++ src/quotes.js
@@ -5,7 +5,7 @@ import API from "./utils/API";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.scss";
 import { faRandom, faInfo } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class User extends React.Component {
   constructor(props) {
@@ -13,15 +13,15 @@ class User extends React.Component {
 
     this.state = {
       isLoading: true,
-      picture: null,
+      picture: null
     };
   }
 
-  getPicture = async () =>{    
-    let picData = await API.get('/', {
+  getPicture = async () => {
+    let picData = await API.get("/", {
       headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
+        "Access-Control-Allow-Origin": "*"
+      }
       // params:{
       //   client_id: '7e844b2d98b1b97763a6ae53ad99b66a5dc338785e48e0f91e60e00c28d82102',
       //   orientation: 'landscape'
@@ -29,41 +29,44 @@ class User extends React.Component {
     });
     //picData = picData.data.urls.regular;
     //picData = picData.data[0].url
-    picData = picData.data[0].download_url
-    console.log(picData);    
-    const picture = picData;    
+    let i = Math.floor(Math.random() * picData.data.length + 1);
+    picData = picData.data[i].download_url;
+    console.log(picData);
+    const picture = picData;
     this.setState({
-      ...this.state, ...{isLoading: false, picture}
-    })
-  }
-  componentDidMount(){
+      ...this.state,
+      ...{ isLoading: false, picture }
+    });
+  };
+  componentDidMount() {
     this.getPicture();
   }
   render() {
     const { picture, isLoading } = this.state;
 
-    const userDetails = (                               
-          <div className="quotes-bg" style={{background:`url(${picture})`}}>
-            <div className="quotes-overlay">
-              <div className="quotes-base text-center">
-                <p className="quote">You only live once, but if you do it right, once is enough.<span className="quote-by">Mae West</span></p>					
-              </div>
-              <div className="shuffle text-center" onClick={this.getPicture}>
-                <FontAwesomeIcon icon={faRandom} />
-              </div>
-              <div className="info-btn">
-                <FontAwesomeIcon icon={faInfo} />
-              </div>
-            </div>          
-        </div>      
+    const userDetails = (
+      <div className="quotes-bg" style={{ background: `url(${picture})` }}>
+        <div className="quotes-overlay">
+          <div className="quotes-base text-center">
+            <p className="quote">
+              You only live once, but if you do it right, once is enough.
+              <span className="quote-by">Mae West</span>
+            </p>
+          </div>
+          <div className="shuffle text-center" onClick={this.getPicture}>
+            <FontAwesomeIcon icon={faRandom} />
+          </div>
+          <div className="info-btn">
+            <FontAwesomeIcon icon={faInfo} />
+          </div>
+        </div>
+      </div>
     );
 
     const loadingMessage = <span className="d-flex m-auto">Loading...</span>;
 
     return (
-      <div className="row">
-        {isLoading ? loadingMessage : userDetails}
-      </div>
+      <div className="row">{isLoading ? loadingMessage : userDetails}</div>
     );
   }
 }
